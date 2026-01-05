327da252fe7de05a08c19ff1284f3ea5643133dc
Dasarath C <robotzeror@gmail.com>
2026-01-06 03:30:17 +0530

    Nothing trying to switch to main

--- lib/screens/alerts_screen.dart
+++ lib/screens/alerts_screen.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-// import 'item_detail_screen.dart';
+import 'item_detail_screen.dart';
 import 'chat_screen.dart';
 
 class AlertsScreen extends StatelessWidget {
